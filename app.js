bb5c18f23b737ea095637eebc30db141163fd5dd
Lê Minh Thành <leminhthanh280425@gmail.com>
2025-12-02 12:32:27 +0700

    Fix: change api /tasks to /words

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors());
 
-app.use('/tasks', taskRouter);
+app.use('/words', taskRouter);
 app.use('/users', userRouter);
 // app.use('/status', statusRouter);
 
